b0b09f3a10ccdd267d8a159cdf8f802f4571a1bc
abdulrahman.hassanin <abdulrahman.hasanin@gmail.com>
2020-01-11 01:20:49 +0200

    Update MidTermProject_Camera_Student.cpp
    DataBuffer

--- src/MidTermProject_Camera_Student.cpp
+++ src/MidTermProject_Camera_Student.cpp
@@ -63,6 +63,8 @@ int main(int argc, const char *argv[])
         DataFrame frame;
         frame.cameraImg = imgGray;
         dataBuffer.push_back(frame);
+        if(dataBuffer.size() < dataBufferSize)
+            dataBuffer.pop_front();
 
         //// EOF STUDENT ASSIGNMENT
         cout << "#1 : LOAD IMAGE INTO BUFFER done" << endl;
